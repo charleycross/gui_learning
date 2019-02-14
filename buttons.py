94109de84c8584e08f0f2154e8769be2fdac8d8b
charleycross <crossch@wgc.school.nz>
2019-02-15 10:09:27 +1300

    initial commit

---
+++ buttons.py
@@ -0,0 +1,45 @@
+import tkinter
+from tkinter import *
+
+
+def print_message():
+    labeltext.set("hello!!")
+
+
+def print_message2():
+    labeltext.set("thanks for clicking :))))")
+
+
+def print_message3():
+    labeltext.set("thanks for clicking again! :)))))))")
+
+
+root = tkinter.Tk()
+
+root.title("Hello world!!!")
+root.geometry("600x400")
+
+labeltext = StringVar()
+labeltext.set("")
+
+
+my_label = tkinter.Label(root)
+my_label.config(textvariable=labeltext)
+my_label.grid()
+
+button = tkinter.Button(root)
+button.config(text="click me!!",
+              fg="purple",
+              command=print_message)
+button.grid()
+
+button2 = tkinter.Button(root)
+button2.config(text="click me now!!", command=print_message2)
+button2.grid()
+
+
+button3 = tkinter.Button(root)
+button3.config(text="click me now!!", command=print_message3)
+button3.grid()
+
+root.mainloop()
